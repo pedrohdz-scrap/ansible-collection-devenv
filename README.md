99faa420be7fe1ade20ab4474dd83d9ea5aff788
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2021-09-26 13:05:51 +0200

    Fixed directory structure.
    Symlink work-around no longer works.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-ansible_collections/avinode/devenv/README.md
+# ansible-collection-devenv
+
+```bash
+python3.9 -m venv .venv
+source .venv/bin/activate
+pip install -U pip
+pip install ansible ansible-lint 'molecule[docker]' yamllint
+
+cat galaxy.yml | ruby -r yaml -r json -e 'puts YAML.load($stdin.read).to_json' \
+    | jq -r '.dependencies | keys | .[]' \
+    | xargs -i ansible-galaxy collection install -p . '{}'
+```
+
+Run tests:
+
+```bash
+cd ansible_collections/avinode/devenv/
+
+ansible-test sanity --docker default
+ansible-test units --docker default
+
+ansible-test integration --docker ubuntu2004 \
+    --python-interpreter /usr/bin/python3
+
+ansible-test integration \
+    --docker geerlingguy/docker-debian10-ansible:latest \
+    --python-interpreter /usr/bin/python3
+```
--- scripts/setup-local-ansible.sh
+++ scripts/setup-local-ansible.sh
@@ -1,23 +1,75 @@
 #! /bin/bash
 
-#set -o xtrace
 set -o errexit
 set -o pipefail
 set -o nounset
 
-INSTALL_DIR=${GITHUB_WORKSPACE:-$PWD}
-VENV_BIN_DIR=${INSTALL_DIR}/.venv/bin
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+PROJECT_COLLECTION_DIR=$(realpath "$PROJECT_DIR/../..")
 
-echo "+ INFO - Setting up the project in: $INSTALL_DIR"
-if [ -e "${INSTALL_DIR}/.venv" ]; then
-  echo "+ ERROR - Already exists: ${INSTALL_DIR}/.venv"
+
+#------------------------------------------------------------------------------
+# Helpers
+#------------------------------------------------------------------------------
+log_section() {
+  (
+    set +o xtrace
+    echo
+    echo "+------------------------------------------------------------------------------"
+    echo "+ $@"
+    echo "+------------------------------------------------------------------------------"
+  )
+}
+
+
+#------------------------------------------------------------------------------
+# Sanity checks
+#------------------------------------------------------------------------------
+log_section "Sanity checks"
+echo "+ INFO - Setting up the project in: $PROJECT_DIR"
+if [ -e "${PROJECT_DIR}/.venv" ]; then
+  echo "+ ERROR - Already exists: ${PROJECT_DIR}/.venv"
   exit 1
 fi
 
-/usr/bin/python3 -m venv "${INSTALL_DIR}/.venv"
-"${VENV_BIN_DIR}/pip3" install -U pip
-"${VENV_BIN_DIR}/pip3" install ansible pylint flake8 pyright mypi junit-xml
-"${VENV_BIN_DIR}/ansible" --version
-ruby -r yaml -r json -e 'puts YAML.load($stdin.read).to_json' < galaxy.yml \
+echo "+ INFO - Ansible collections are in: $PROJECT_COLLECTION_DIR"
+_expected_project_collection_dir=$(realpath "$PROJECT_COLLECTION_DIR/..")/ansible_collections
+if [ "$PROJECT_COLLECTION_DIR" != "$_expected_project_collection_dir" ]; then
+  echo "+ ERROR - Parent directory must '$_expected_project_collection_dir', not: $PROJECT_COLLECTION_DIR"
+  exit 1
+fi
+
+set -o xtrace
+
+
+#------------------------------------------------------------------------------
+# Install Python virtual env
+#------------------------------------------------------------------------------
+log_section "Setup Python virtual environment"
+/usr/bin/python3 -m venv "${PROJECT_DIR}/.venv"
+# shellcheck disable=SC1091
+source "${PROJECT_DIR}/.venv/bin/activate"
+pip3 install --upgrade --upgrade-strategy eager wheel pip
+pip3 install ansible ansible-lint flake8 junit-xml mypi pylint pyright yamllint
+
+
+#------------------------------------------------------------------------------
+# Display setup
+#------------------------------------------------------------------------------
+log_section "Show configuration"
+which ansible ansible-galaxy ansible-lint yamllint
+ansible --version
+ansible-galaxy --version
+ansible-lint --version
+yamllint --version
+
+
+#------------------------------------------------------------------------------
+# Install Ansible collections
+#------------------------------------------------------------------------------
+log_section "Install Ansible collections"
+# shellcheck disable=SC2016
+ruby -r yaml -r json -e 'puts YAML.load($stdin.read).to_json' < ${PROJECT_DIR}/galaxy.yml \
     | jq -r '.dependencies | keys | .[]' \
-    | xargs -I '{}' "${VENV_BIN_DIR}/ansible-galaxy" collection install -p . '{}'
+    | xargs -I '{}' ansible-galaxy collection install --force --upgrade \
+        --collections-path "$PROJECT_COLLECTION_DIR" '{}'
