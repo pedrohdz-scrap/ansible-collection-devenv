658dd1f94f5264b16287b56cb6f68fbfdd3d4f7f
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2022-08-08 08:32:05 +0200

    Fixed baseline CI checks and upgraded Ansible

--- README.md
+++ README.md
@@ -22,18 +22,39 @@ Run tests:
 
 ```bash
 cd ansible-collection-devenv/ansible_collections/avinode/devenv
+source ./.venv/bin/activate
 
-ansible-test sanity --color --docker default \
+ansible-test sanity --color \
+    --docker geerlingguy/docker-debian11-ansible:latest \
+    --python 3.9 \
     --exclude scripts/setup-local-ansible.sh \
     --exclude scripts/setup-macports.sh \
     --exclude .gitignore
 
-ansible-test units --color --docker default
-
-ansible-test integration --color --docker ubuntu2004 \
-    --python-interpreter /usr/bin/python3
+ansible-test units --color \
+    --docker geerlingguy/docker-debian11-ansible:latest \
+    --python 3.9
 
 ansible-test integration --color \
-    --docker geerlingguy/docker-debian10-ansible:latest \
-    --python-interpreter /usr/bin/python3
+    --docker geerlingguy/docker-debian11-ansible:latest \
+    --docker-privileged \
+    --python 3.9
+
+ansible-test integration --color \
+    --docker geerlingguy/docker-ubuntu2004-ansible \
+    --docker-privileged \
+    --python 3.8
+
+# TODO - Not suported by ansible-test.  Should be in the next ansible-test
+# release.
+#ansible-test integration --color \
+#    --docker geerlingguy/docker-ubuntu2204-ansible \
+#    --docker-privileged \
+#    --python 3.10
+
+# TODO - Broken.  Need to come back to this.
+#ansible-test integration --color \
+#    --docker geerlingguy/docker-debian10-ansible:latest \
+#    --docker-privileged \
+#    --python 3.7
 ```
--- scripts/setup-macports.sh
+++ scripts/setup-macports.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
+#! /bin/bash
 
 #set -o xtrace
 set -o errexit
 set -o pipefail
 set -o nounset
 
+DISABLE_SPOTLIGHT="false"
+while true; do
+  case "${1:-}" in
+    --stop-spotlight )
+      DISABLE_SPOTLIGHT="true"
+      shift 1
+      ;;
+    * ) break ;;
+  esac
+done
 
 #------------------------------------------------------------------------------
 # Helper functions
@@ -14,17 +24,28 @@ function log() {
 }
 
 function get_latest_macports_version() {
+  local _auth_header
+  if [[ -n "${GITHUB_TOKEN:-}" ]]; then
+    _auth_header=(--header "authorization: Bearer $GITHUB_TOKEN")
+  fi
   /usr/bin/curl \
-      --fail --silent \
+      --fail --silent "${_auth_header[@]}" \
       --header 'Accept: application/vnd.github.v3+json' \
-      --header "authorization: Bearer $GITHUB_TOKEN" \
       'https://api.github.com/repos/macports/macports-base/releases/latest' \
-    | /usr/local/bin/jq --raw-output '.tag_name' \
+    | /usr/bin/python3 -c "import sys, json; print(json.load(sys.stdin)['tag_name'])" \
     | /usr/bin/sed -e 's/^v//'
 }
 
 function get_macos_version() {
-  /usr/bin/sw_vers -productVersion | /usr/bin/sed -E 's/\.[0-9]+$//'
+  local _version
+  local _version_parts
+  _version=$(/usr/bin/sw_vers -productVersion)
+  IFS='.' read -r -a _version_parts <<< "$_version"
+  if [[ "${_version_parts[0]}" -lt 11 ]]; then
+    echo "${_version_parts[0]}.${_version_parts[1]}"
+  else
+    echo "${_version_parts[0]}"
+  fi
 }
 
 function get_macos_friendly_name() {
@@ -33,7 +54,9 @@ function get_macos_friendly_name() {
       '/SOFTWARE LICENSE AGREEMENT FOR macOS/' \
       '/System/Library/CoreServices/Setup Assistant.app/Contents/Resources/en.lproj/OSXSoftwareLicense.rtf' \
     | /usr/bin/awk -F 'macOS ' '{print $NF}' \
-    | /usr/bin/awk '{print substr($0, 0, length($0)-1)}'
+    | /usr/bin/awk '{print substr($0, 0, length($0)-1)}' \
+    | /usr/bin/tr -d '[:space:]'
+
 }
 
 function get_macports_pkg_file_name() {
@@ -44,8 +67,10 @@ function get_macports_pkg_file_name() {
 #------------------------------------------------------------------------------
 # Main
 #------------------------------------------------------------------------------
-log 'Disabeling Spotlight Search indexing'
-/usr/bin/sudo mdutil -a -i off
+if [ "${DISABLE_SPOTLIGHT}" == "true" ]; then
+  log 'Disabeling Spotlight Search indexing'
+  /usr/bin/sudo mdutil -a -i off
+fi
 
 log 'Installation information...'
 MACPORTS_PKG_FILE="$(get_macports_pkg_file_name)"
@@ -60,8 +85,15 @@ log 'Downloading MacPorts...'
     --silent \
     "https://distfiles.macports.org/MacPorts/$MACPORTS_PKG_FILE"
 
+if ! sudo -n true 2>/dev/null; then
+  echo "======================================================="
+  echo "== Asking for password in order to update Mac Ports ==="
+  echo "======================================================="
+fi
+
 log 'Installing MacPorts...'
 /usr/bin/sudo /usr/sbin/installer -pkg "$MACPORTS_PKG_FILE" -target /
+rm "$MACPORTS_PKG_FILE"
 export PATH="/opt/local/libexec/gnubin:/opt/local/bin:/opt/local/sbin:$PATH"
 hash -r
 
@@ -70,6 +102,21 @@ log 'Running MacPorts selfupdate/upgrade...'
 /usr/bin/sudo /opt/local/bin/port upgrade outdated
 
 log 'Install minimum required MacPorts...'
-/usr/bin/sudo /opt/local/bin/port install coreutils python39 python3_select python_select
+/usr/bin/sudo /opt/local/bin/port -N install \
+    coreutils \
+    curl-ca-bundle \
+    pip_select \
+    py39-certifi \
+    py39-distlib \
+    py39-pip \
+    py39-setuptools \
+    py39-virtualenv \
+    py39-wheel \
+    python39 \
+    python3_select \
+    python_select
+
 /usr/bin/sudo /opt/local/bin/port select python3 python39
 /usr/bin/sudo /opt/local/bin/port select python python39
+/usr/bin/sudo /opt/local/bin/port select pip3 pip39
+/usr/bin/sudo /opt/local/bin/port select pip pip39
